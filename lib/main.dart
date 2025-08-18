74b467cf3c907078d8461c944f63c3637ac3f028
Sojib201 <sojib.cse3729@gmail.com>
2025-08-18 22:03:02 +0600

    project setup

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'MY FLYN',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF4B1D8B)),
